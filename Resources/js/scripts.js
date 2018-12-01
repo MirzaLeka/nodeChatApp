af94e17b89e7bca782bacdbf10fecd11363fdfab
Mirza Leka <mirza_376@hotmail.com>
2018-12-01 23:25:38 +0100

    Updated package.json. Changed scripts.js file

--- Resources/js/scripts.js
+++ Resources/js/scripts.js
@@ -4,14 +4,3 @@ import '../sass/styles.scss';
 import 'normalize.css/normalize.css';
 
 // sayHello();
-
-document.onload = () => {
-
-  fetch('/users')
-    .then(res => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-
-
-};
